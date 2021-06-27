95ef3be0a785be4ea94ff3a5b820253caff39141
Mary Mettille <mary.mettille.brist@gmail.com>
2021-06-26 22:09:35 -0500

    Looping through worked, but the app did crash with an error of Cannot set headers after they are sent to the client

--- server/routes/query.router.js
+++ server/routes/query.router.js
@@ -31,15 +31,22 @@ router.post('/trigger', (req, res) => {
     // POST route code here
     console.log(req.body)
     console.log(req.body[0].trigger)
-    const insertTriggerQuery = `
-        INSERT INTO "trigger_input" ("trigger", "user_id")
-        VALUES ($1, $2)`;
-    pool.query(insertTriggerQuery, [req.body[0].trigger, req.user.id]).then(result => {
-        res.sendStatus(201);
-    }).catch(err => {
-        console.log(err);
-        res.sendStatus(500)
-    })
+    const array = req.body;
+    // This is close. What it will do is make ONE query with the separate values of what the user inputs.
+    let sql = array.map(trigger => trigger.trigger)
+    console.log(sql)
+    // What if I loop through everything instead? That would send multiple, wouldn't it?
+    for(let item of array){
+            const insertTriggerQuery = `
+            INSERT INTO "trigger_input" ("trigger", "user_id")
+            VALUES ($1, $2)`;
+        pool.query(insertTriggerQuery, [item.trigger, req.user.id]).then(result => {
+            res.sendStatus(201);
+        }).catch(err => {
+            console.log(err);
+            res.sendStatus(500)
+        })
+    }
 });
 
 module.exports = router;

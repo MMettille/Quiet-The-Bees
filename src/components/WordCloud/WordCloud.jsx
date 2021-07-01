5433365ccfddb1cc3a2f14c56fa044dccbc5d17b
Mary Mettille <mary.mettille.brist@gmail.com>
2021-07-01 17:27:35 -0500

    We Have a Working WordCloud! Now to customizable colors

--- src/components/WordCloud/WordCloud.jsx
+++ src/components/WordCloud/WordCloud.jsx
@@ -7,88 +7,40 @@ import * as am4plugins_wordCloud from "@amcharts/amcharts4/plugins/wordCloud";
 
 import Header from '../Header/Header'
 import { stringifyRequest } from "loader-utils";
+import { tag } from "postcss-selector-parser";
 function WordCloud() {
-
-    // ⬇ Creating the chart
-    const chart = useRef(null);
 
     useEffect(() => {
       // ⬇ This calls my get request from the server
-    //   getWordCloud();
-    getStringOfWords()
+    getWordCloud()
     }, []);
 
-    const getStringOfWords = () => {
+    const getWordCloud = () => {
         axios.get("/api/query/wordcloud")
         .then((response) => {
             const words = response.data;
             console.log(words)
 
-            // // ⬇ This creates the kind of chart that I would like from am4charts
-            // const x = am4core.create("chartdiv", am4plugins_wordCloud.WordCloud );
-            // // ⬇ Creating the series for a word cloud
-            // const series = chart.series.push(new am4plugins_wordCloud.WordCloudSeries());
-
+            // ⬇ This creates the kind of chart that I would like from am4charts
+            const x = am4core.create("chartdiv", am4plugins_wordCloud.WordCloud );
+            // ⬇ Creating the series for a word cloud
+            const series = x.series.push(new am4plugins_wordCloud.WordCloudSeries());
+            // ⬇ Binding the series to the data we grabbed from the database 
+            series.data = words;
+            series.dataFields.word = "tag";
+            series.dataFields.value = "weight";
+            // ⬇ Customizing the color
+            series.colors = new am4core.ColorSet();
+            series.colors.passOptions = {};
         }).catch((error) => {
             console.log(`We have a server error`, error);
           });
     }
 
-    // ⬇ This gets my data from the database and sets it to feedback
-    // const getWordCloud = () => {
-    //   axios
-    //     .get("/api/query/spoongraph")
-    //     .then((response) => {
-    //       const spoonData = response.data;
-          
-  
-    // //       // ⬇ This creates the kind of chart that I would like from am4charts
-    //       let x = am4core.create("chartdiv", am4charts.XYChart);
-    //       // ⬇ Padding to the right of the graph
-    //       x.paddingRight = 20;
-    //       x.paddingLeft = 20;
-    //       // ⬇ This declares what kind of date format I would like.
-    //       x.dateFormatter.dateFormat = "yyyy-MM-dd";
-    //       // ⬇ Adding from the data that I set in the getFeedback function
-    //       let data = spoonData
-    //       // ⬇ Making the data tied to the chart, called x.
-    //       x.data = data;
-    //       // ⬇ creating xAxes (the horizontal axis)
-    //       let dateAxis = x.xAxes.push(new am4charts.DateAxis());
-    //       dateAxis.title.text = "Date";
-    //       dateAxis.renderer.grid.template.location = 0;
-    //       // ⬇ creating yAxes (the vertical axis)
-    //       let valueAxis = x.yAxes.push(new am4charts.ValueAxis());
-    //       valueAxis.title.text = "Spoons"
-    //       valueAxis.tooltip.disabled = true;
-    //       valueAxis.renderer.minWidth = 35;
-    //       valueAxis.min = 0;
-    //       valueAxis.max = 10;
-    //       valueAxis.extraMin = 0.1;
-    //       valueAxis.extraMax = 0.1; 
-    //       // ⬇ Creating the series for a line graph
-    //       let series = x.series.push(new am4charts.LineSeries());
-    //       // ⬇ Binding the data to the series
-    //       series.dataFields.dateX = "date";
-    //       series.dataFields.valueY = "spoon";
-    //       series.tooltipText = "{valueY.value}";
-    //       x.cursor = new am4charts.XYCursor();
-    //       // ⬇ Scrollbar functionality at the top of the graph
-    //       let scrollbarX = new am4charts.XYChartScrollbar();
-    //       scrollbarX.series.push(series);
-    //       x.scrollbarX = scrollbarX;
-  
-    //       chart.current = x;
-  
-    // //     })
-  
-    // // }
-
-
   return (
     <>
         <Header />
-        {/* <div id="chartdiv"></div> */}
+        <div id="chartdiv"></div>
     </>
   );
 }

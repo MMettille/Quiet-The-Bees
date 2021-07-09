6be6d28702780236ba5810a7d831a1b1ad015a47
Mary Mettille <mary.mettille.brist@gmail.com>
2021-07-09 01:02:41 -0500

    Added About Page Text

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import List from '@material-ui/core/List';
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -9,7 +10,33 @@ function AboutPage() {
   return (
     <div className="container">
       <div>
-        <p>This about page is for anyone to read!</p>
+        <p>This applicaiton was made in two weeks with these technologies:</p>
+        <List>
+          <li>React</li>
+          <li>Redux</li>
+          <li>Redux-Saga</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Javascript</li>
+          <li>Node.js</li>
+          <li>Express</li>
+          <li>PostgreSQL</li>
+          <li>am4charts</li>
+          <li>Material-ui</li>
+          <li>react-masonry-css</li>
+        </List>
+        <br />
+        <p>A huge shoutout to my instructor, Dane. Thank you for downloading all this
+          code into my brain and for talking things out with me when the bees buzzed too
+          loud.
+        </p>
+        <br />
+        <p>And to my co-parent and partner, Isaac - thank you for being you. Word cannot express 
+          how thankful I am that you were willing to take the large brunt of parenting our tiny
+          hoomans while I furthered my career.
+        </p>
+        <br />
+        <p>Have any feedback to give on this app? Please email me at mary.mettille.brist@gmail.com.</p>
       </div>
     </div>
   );
